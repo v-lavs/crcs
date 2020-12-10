1b4cd1e74fb07aa33d12bd2fa68fc12fea6d8173
v-lavs <lavskavanda@gmail.com>
2020-12-10 18:54:57 +0200

    added section-intro animation

---
+++ src/js/main.js
@@ -0,0 +1,60 @@
+/*
+* to include js file write: `//= include ./path-to-file`
+* */
+
+//= include ../../node_modules/jquery/dist/jquery.js ;
+
+
+// CUSTOM SCRIPTS
+
+$(document).ready(function () {
+
+    //MOBILE MENU
+    var nav = $('.header__nav');
+
+    $('.btn-burger').click(function (e) {
+        e.preventDefault();
+        nav.addClass('open');
+        jQuery('.backdrop').fadeIn();
+    });
+
+    $('.btn-close, .backdrop').click(function (e) {
+        e.preventDefault();
+        nav.removeClass('open');
+        jQuery('.backdrop').fadeOut();
+    });
+
+
+    //HEADER SCROLL
+
+    function onHeaderScrol() {
+        scrolled = window.pageYOffset || document.documentElement.scrollTop;
+        if (scrolled > 40) {
+            jQuery(".header").addClass('header_active');
+        } else {
+            jQuery(".header").removeClass('header_active');
+        }
+    }
+
+    $(document).on('scroll', function () {
+        onHeaderScrol()
+    });
+
+//MOUSE-PARALLAX
+    $('.mouse-parallax').on('mousemove', (e) => {
+        const x = e.clientX / $(window).width();
+        const y = e.clientY / $(window).height();
+        const $activeSection = $(e.currentTarget);
+
+        $activeSection.find('.mouse-parallax__bg').css(
+            'transform',
+            'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)',
+
+        );
+        $activeSection.find('.mouse-parallax__bg-2').css(
+            'transform',
+            'translate(+' + x * 50 + 'px, +' + y * 50 + 'px)',
+        );
+    });
+
+});
